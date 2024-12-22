7a370cd10588cab924de6cf26d16cdc6fb8db0df
shreyya18 <shreyapoojary251@gmail.com>
2024-12-22 19:42:37 +0530

    update

--- react-basics/basics/src/main.jsx
+++ react-basics/basics/src/main.jsx
@@ -1,11 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import MyButton from './MyButton.jsx'
+import MyApp from './MyApp.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <MyButton />
-    <MyButton/>
+    <MyApp/>
   </StrictMode>,
 )
