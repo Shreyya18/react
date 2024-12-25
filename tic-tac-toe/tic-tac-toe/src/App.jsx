63a8aad2369b4e2b772760805fbf053bd57ad045
shreyya18 <shreyapoojary251@gmail.com>
2024-12-25 18:03:59 +0530

    added interactivity

--- tic-tac-toe/tic-tac-toe/src/App.jsx
+++ tic-tac-toe/tic-tac-toe/src/App.jsx
@@ -1,30 +1,36 @@
-function Square({ value }) {
-  function handleClick() {
-    console.log("clicked");
-  }
+import { useState } from "react";
+function Square({value,onSquareClick}) {
+  
   return (
-    <button className="square" onClick={handleClick}>
+    <button className="square" onClick={onSquareClick}>
       {value}
     </button>
   );
 }
 export default function Board() {
+  const [squares, setSquares] = useState(Array(9).fill(null));
+
+  function handleClick() {
+    const nextSquares= squares.slice();
+    nextSquares[i]="X";
+    setSquares(nextSquares);
+  }
   return (
     <>
       <div className="board-row">
-        <Square value="1" />
-        <Square value="2" />
-        <Square value="3" />
+        <Square value={squares[0] onSquareClick={()=>handleClick(0)} />
+        <Square value={squares[1]} />
+        <Square value={squares[2]} />
       </div>
       <div className="board-row">
-        <Square value="4" />
-        <Square value="5" />
-        <Square value="6" />
+        <Square value={squares[3]} />
+        <Square value={squares[4]} />
+        <Square value={squares[5]} />
       </div>
       <div className="board-row">
-        <Square value="7" />
-        <Square value="8" />
-        <Square value="9" />
+        <Square  value={squares[6]} />
+        <Square value={squares[7]} />
+        <Square value={squares[8]} />
       </div>
     </>
   );
