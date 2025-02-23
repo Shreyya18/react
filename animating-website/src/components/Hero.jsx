076a73d7fbe6c2b53deda5ed203d790d335bd4b6
shreyya18 <shreyapoojary251@gmail.com>
2025-02-23 20:25:20 +0530

    create new div

--- animating-website/src/components/Hero.jsx
+++ animating-website/src/components/Hero.jsx
@@ -2,7 +2,15 @@ import React from 'react'
 
 const Hero = () => {
   return (
-    <div className='relative h-dvh w-screen overflow-x-hidden'>Hero</div>
+    <div className='relative h-dvh w-screen overflow-x-hidden'>
+      <div id="video-frame" className="relative z-10 h-dvh w-screen">
+        <div>
+          <div className='mask-clip-path absolute-cnter absolute z-50 size-64 cursor-pointer overflow-hidden rounded-lg'>
+            <div>MiniVideoPlayer</div>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
