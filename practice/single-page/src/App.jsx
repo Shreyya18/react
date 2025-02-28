7d543e89578f87724692afe4c3b224201891ff49
shreyya18 <shreyapoojary251@gmail.com>
2025-02-28 22:03:36 +0530

    add initial HTML, CSS, and React components for single-page application setup

---
+++ practice/single-page/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './App.css'
+
+const App = () => {
+  return (
+    <div className='bg-royalviolet min-h-screen  '>
+       
+    </div>
+  )
+}
+
+export default App
