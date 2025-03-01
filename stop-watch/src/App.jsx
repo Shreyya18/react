3cdb740014c77c41148956c7bc5147840a99c34d
shreyya18 <shreyapoojary251@gmail.com>
2025-03-01 15:13:02 +0530

    add App component to render StopWatch in the application

---
+++ stop-watch/src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import './App.css'
+import StopWatch from "./Components/StopWatch";
+const App=()=>{
+
+  return(
+    <div className="bg-black min-h-screen">
+      <StopWatch />
+    </div>
+
+  )
+}
+
+export default App;
