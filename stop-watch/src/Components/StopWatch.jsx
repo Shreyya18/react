a3f64d388672265b6d8abc43c9c673acdaf9b5e5
shreyya18 <shreyapoojary251@gmail.com>
2025-03-01 21:00:46 +0530

    implement stopwatch functionality with start, pause, and reset actions

--- stop-watch/src/Components/StopWatch.jsx
+++ stop-watch/src/Components/StopWatch.jsx
@@ -3,11 +3,41 @@ import Timer from "./Timer";
 import ControlButton from "./ControlButton";
 
 const StopWatch = () => {
+  const[isActive,setIsActive]=React.useState(false);
+  const[isPaused,setIsPaused]=React.useState(false);
+  const[time,setTime]=React.useState(0);
+
+  const handleStart=()=>{
+    setIsActive(true);
+
+  }
+
+  const handleReset=()=>{
+    setIsActive(false);
+  }
+  const handlePause=()=>{
+    setIsPaused(!isPaused);
+
+  }
+
+  React.useEffect(()=> {
+    let intervalId;
+    if(isActive && !isPaused){
+      intervalId=setInterval(()=>{
+        setTime((time)=>time+1);
+      },10);
+    }
+    else{
+      clearInterval(intervalId);
+    }
+  },[isActive,isPaused] );
+
+
   return (
-    <div className="h-screen w-full flex flex-col items-center  m-12 bg-black text-white mx-auto rounded-lg">
+    <div className="min-h-screen w-full flex flex-col items-center  m-12 bg-white text-black mx-auto  ">
       <h1 className="m-4 text-3xl font-bold text-red-700">Stop Watch</h1>
-      <Timer />
-      <ControlButton />
+      <Timer times={time} />
+      <ControlButton active={isActive} onStart={handleStart} onReset={handleReset} onPause={handlePause} pause={isPaused}/>
     </div>
   );
 };
