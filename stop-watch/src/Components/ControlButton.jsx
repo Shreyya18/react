6d7d334e03033f99f7e48639c2de56a7d01eb1a6
shreyya18 <shreyapoojary251@gmail.com>
2025-03-01 21:00:41 +0530

    refactor ControlButton to handle start, pause, and reset actions with dynamic button rendering

--- stop-watch/src/Components/ControlButton.jsx
+++ stop-watch/src/Components/ControlButton.jsx
@@ -1,11 +1,22 @@
 import React from 'react'
 
-const ControlButton = () => {
-  return (
+const ControlButton = ({active,onStart,onPause,pause, onReset}) => {
+
+  const StartButton= (
     <div>
-        <button className='mt-20 px-4 py-1 rounded-sm text-white bg-red-500 hover:bg-red-700 cursor-pointer'>Start</button>
+        <button className='mt-20 px-4 py-1 rounded-sm text-white bg-red-500 hover:bg-red-700 cursor-pointer' onClick={onStart}>Start</button>
     </div>
+  );
+  const ActiveButtons=(
+    <div className='flex space-x-2'>
+      <button className='mt-20 bg-red-500 cursor-pointer hover:bg-red-700 px-4 py-1 text-white rounded w-24' onClick={onReset}>Reset</button>
+      <button className='mt-20 bg-red-500 cursor-pointer hover:bg-red-700 px-4 py-1 text-white rounded w-24' onClick={onPause}>{pause?"Resume":"Pause"}</button>
+    </div>
+  );
+  
+  return(
+    <div>{active ? ActiveButtons:StartButton}</div>
   )
 }
 
-export default ControlButton
+export default ControlButton;
