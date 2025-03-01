23ca52b1607a207591c04ba1ac2fc71a795c2135
shreyya18 <shreyapoojary251@gmail.com>
2025-03-01 21:00:52 +0530

    refactor Timer component to accept times prop and display formatted time

--- stop-watch/src/Components/Timer.jsx
+++ stop-watch/src/Components/Timer.jsx
@@ -1,12 +1,18 @@
 import React from 'react'
 
-const Timer = () => {
-    const[seconds,setSeconds]=React.useState("00");
-    const[minutes,setMinutes]=React.useState("00");
-    const[hours,setHours]=React.useState("00");
+const Timer = ({times}) => {
+ 
     return (
     <div>
-        <p className='text-3xl'>{hours}:{minutes}:{seconds}</p>
+      <span >
+                {("0" + Math.floor((times.time / 60000) % 60)).slice(-2)}:
+            </span>
+            <span >
+                {("0" + Math.floor((times.time / 1000) % 60)).slice(-2)}.
+            </span>
+            <span >
+                {("0" + ((times.time / 10) % 100)).slice(-2)}
+            </span>
     </div>
   )
 }
