84104cdd122254f43cba7088f4ff9ff0ce8dd254
shreyya18 <shreyapoojary251@gmail.com>
2025-03-01 21:26:33 +0530

    fix Timer component to correctly format time display

--- stop-watch/src/Components/Timer.jsx
+++ stop-watch/src/Components/Timer.jsx
@@ -7,11 +7,11 @@ const Timer = ({times}) => {
       <span >
                 {("0" + Math.floor((times.time / 60000) % 60)).slice(-2)}:
             </span>
-            <span >
-                {("0" + Math.floor((times.time / 1000) % 60)).slice(-2)}.
+            <span className="digits">
+                {("0" + Math.floor((times.time / 100) % 60)).slice(-2)}:
             </span>
-            <span >
-                {("0" + ((times.time / 10) % 100)).slice(-2)}
+            <span  >
+                {("0" + ((times.time / 1) % 100)).slice(-2)}
             </span>
     </div>
   )
